bc1c07d554342052b0b674426b18cc182239df80
16850 <1685003606@qq.com>
2021-09-20 21:28:25 +0800

    8th test commit

--- src/www/Ning/FirstProgrammingJob/Grade_two.java
+++ src/www/Ning/FirstProgrammingJob/Grade_two.java
@@ -5,14 +5,15 @@ public class Grade_two {
         StringBuffer sb = new StringBuffer(str);
         String[] flag = str.split("switch");
         int SWITCHSUM = flag.length - 1;
-        int aaa = 0;
         int[] arr;
         int i = 0;
         arr = new int[SWITCHSUM];
         int[] casenumarr;
         casenumarr = new int[SWITCHSUM];
+        int[] caseside;
+        caseside = new int[30];
         int index=-1;
-        do{
+        do{                       //记录switch的下标
             index=str.indexOf("switch",index+1);
             if(index!=-1){
                 arr[i] = index ;
@@ -20,15 +21,34 @@ public class Grade_two {
             }
         }while(index!=-1);
 
+        int j =0;
 
-        do{                            //
-            index=str.indexOf("switch",index+1);
+        do{                            //记录case的下标
+            index=str.indexOf("case",index+1);
             if(index!=-1){
-                arr[i] = index ;
-                i++;
+
+                caseside[j] = index;
+                j++;
+
             }
         }while(index!=-1);
 
+        int z =0;
+        int flag1 = 0;
+
+        for (int k = 0; k < caseside.length; k++) {
+
+            if(caseside[k] < arr[flag1+1]){
+                z++;
+            }
+            else
+            {
+                flag1++;
+                z=0;
+                k = k-1;
+            }
+            casenumarr[flag1] = z;
+        }
 
         System.out.println("switch num:"+SWITCHSUM);
         System.out.print("case num:");
